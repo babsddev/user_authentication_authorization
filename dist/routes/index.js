c1c2fc9935c4f93f2ec9a5035355ee18380559b7
babsddev <ajayi.gbemi0806@gmail.com>
2022-10-27 18:01:30 +0100

    commit

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -4,7 +4,7 @@ const express_1 = require("express");
 const router = (0, express_1.Router)();
 /* GET home page. */
 router.get('/', function (_req, res) {
-    res.json({ title: 'Welcome to Airtime To Cash App' });
+    res.json({ title: '' });
 });
 exports.default = router;
 //# sourceMappingURL=index.js.map
--- src/routes/role.ts
+++ src/routes/role.ts
@@ -1,18 +1,18 @@
-import { Router } from "express";
+import { Router } from 'express'
 import {
   createUserRole,
   updateRole,
   deleteRole,
   getAllRoles,
   getRoleById,
-} from "../controllers/roleController";
-import { auth } from "../middleware/auth";
-const router = Router();
+} from '../controllers/roleController'
+import { auth } from '../middleware/auth'
+const router = Router()
 
-router.post("/create", auth, createUserRole);
-router.patch("/edit/:id", auth, updateRole);
-router.get("/all", auth, getAllRoles);
-router.get("/user/:id", auth, getRoleById);
-router.delete("/delete/:id", deleteRole);
+router.post('/create', auth, createUserRole)
+router.patch('/edit/:id', auth, updateRole)
+router.get('/all', auth, getAllRoles)
+router.get('/user/:id', auth, getRoleById)
+router.delete('/delete/:id', deleteRole)
 
-export default router;
+export default router
