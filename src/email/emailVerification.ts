c339e1485d8e706ed9ad526a0f6d507205635421
babsddev <ajayi.gbemi0806@gmail.com>
2022-10-27 18:04:21 +0100

    commit

--- src/email/emailVerification.ts
+++ src/email/emailVerification.ts
@@ -1,9 +1,9 @@
 export function emailVerificationView(token: string): string {
-  const link = `${process.env.BACKEND_URL}/users/verify/${token}`;
+  const link = `${process.env.BACKEND_URL}/users/verify/${token}`
   let temp = `
      <div style="max-width: 700px;
      margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
-     <h2 style="text-align: center; text-transform: uppercase;color: teal;">Welcome to Airtime to Cash POD G.</h2>
+     <h2 style="text-align: center; text-transform: uppercase;color: teal;">Verify Email</h2>
       <p>Hi there, Follow the link by clicking on the button to verify your email
       </p>
        <a href=${link}
@@ -11,12 +11,12 @@ export function emailVerificationView(token: string): string {
         padding: 10px 20px; margin: 10px 0;
        display: inline-block;">Click here</a>
       </div>
-      `;
-  return temp;
+      `
+  return temp
 }
 
 export function forgotPasswordVerification(id: string): string {
-  const link = `${process.env.FRONTEND_URL}/forgot-password/${id}`;
+  const link = `${process.env.FRONTEND_URL}/forgot-password/${id}`
   let temp = `
      <div style="max-width: 700px;
      margin:auto; border: 10px solid #ddd; padding: 50px 20px; font-size: 110%;">
@@ -30,6 +30,6 @@ export function forgotPasswordVerification(id: string): string {
         padding: 10px 20px; margin: 10px 0;
        display: inline-block;">Click here</a>
       </div>
-      `;
-  return temp;
+      `
+  return temp
 }
